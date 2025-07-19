7acecf577f034d0b4e92ca0b393cbc38380bf7e7
Arzoo Shamim <166494063+arzooshamim13@users.noreply.github.com>
2025-07-19 05:06:35 +0500

    Add files via upload

---
+++ First Assignment/app.js
@@ -0,0 +1,86 @@
+console.log("Hello Arzoo");
+
+// 1st Question
+// var num1 = +prompt("Enter first number");
+// var num2 = +prompt("Enter second number");
+// var total = parseInt(num1) + parseInt(num2);
+// console.log("Total is: " + total);
+
+// 2nd Question
+// var num1 = +prompt("Enter first number");
+// var num2 = +prompt("Enter second number");
+// var num3 = +prompt("Enter third number");
+// var total = parseInt(num1) + parseInt(num2) + parseInt(num3);
+// console.log("Total is: " + total);
+
+// 3rd Question
+// var num1 = +prompt("Enter first number");
+// var num2 = +prompt("Enter second number");
+// var total = parseInt(num1) + parseInt(num2);
+// alert("Total is: " + total);
+
+// 4th Question
+
+// var num1 = +prompt("Enter first number");
+// var num2 = +prompt("Enter second number");
+// var plus = parseInt(num1) + parseInt(num2);
+// var minace = parseInt(num1) - parseInt(num2);
+// var multiply = parseInt(num1) * parseInt(num2);
+// var divide = parseInt(num1) / parseInt(num2);
+// console.log(plus);
+// console.log(minace);
+// console.log(multiply);
+// console.log(divide);
+
+// 5th Question
+
+// var user = prompt("Enter Your Name");
+// var num1 = +prompt("Enter first number");
+// var num2 = +prompt("Enter second number");
+// var total = parseInt(num1) + parseInt(num2);
+// console.log("Asslamualikum " + user);
+// console.log("Total Is " + total);
+
+// 6th Question
+// var num1 = prompt("Enter first number");
+// var num2 = prompt("Enter second number");
+// var total = parseInt(num1) + parseInt(num2);
+// alert("Total is: " + total);
+
+// 7th Question
+// var num1 = prompt("Enter first number:");
+// var num2 = prompt("Enter second number:");
+
+// var number1 = parseInt(num1);
+// var number2 = parseInt(num2);
+// if (!isNaN(number1) && !isNaN(number2)) {
+//   var sum = number1 + number2;
+//   console.log("The sum is: " + sum);
+// } else {
+//   console.log("Please enter valid number");
+// }
+
+// 8th Question
+// var num1 = prompt("Enter first number");
+// var add = parseInt(num1) + 10;
+// console.log(num1 + " + 10 = " + add);
+
+// 9th Question
+// let num1 = prompt("Enter first number:");
+// let num2 = prompt("Enter second number:");
+
+// let number1 = parseInt(num1);
+// let number2 = parseInt(num2);
+
+// let sum = number1 + number2;
+
+// if (sum % 2 === 0) {
+//   console.log("The sum is Even: " + sum);
+// } else {
+//   console.log("The sum is Odd: " + sum);
+// }
+
+// 10th Question
+// 
+
+// Assignment Completed
